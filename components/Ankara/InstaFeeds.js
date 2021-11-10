632458113fcf71e48068cd3d9769713e2330287a
luysmarcm <luysmar27@gmail.com>
2021-11-10 13:30:39 -0400

    modificaciones

--- components/Ankara/InstaFeeds.js
+++ components/Ankara/InstaFeeds.js
@@ -1,34 +1,21 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import Feed from "./Feed";
 
 const InstaFeeds = ({ ...props }) => {
 	const [feeds, setFeedsData] = useState([]);
 
 	useEffect(() => {
-		async function fetchInstagramPost() {
-			try {
-
-				fetch(
-					`https://graph.instagram.com/me/media?fields=id,media_type,media_url,permalink,caption&limit=${props.limit}&access_token=IGQVJYNEQwYVBHQXRCWXRuM3BjdllibTk2N3huVE11OTNhLTVqa1E4RENhSzMxWHlmUzNwRTlvUFBNNS1SNU9BLTRxNHNsc01iVDEtYU51eF9aQnRlTTZAWSWdpREdUOG5jbmxxT2FTejRjRU1vUUhldAZDZD`
-				)
-					.then((res) => {
-						return res.json();
-					})
-					.then((data) => {
-						console.log(data.data);
-						setFeedsData(data.data)
-					});
-				
-			} catch (err) {
-				console.log("error", err);
-			}
-		}
-
-		fetchInstagramPost();
+		fetch(
+			`https://graph.instagram.com/me/media?fields=id,media_type,media_url,permalink,caption&limit=${props.limit}&access_token=IGQVJYNEQwYVBHQXRCWXRuM3BjdllibTk2N3huVE11OTNhLTVqa1E4RENhSzMxWHlmUzNwRTlvUFBNNS1SNU9BLTRxNHNsc01iVDEtYU51eF9aQnRlTTZAWSWdpREdUOG5jbmxxT2FTejRjRU1vUUhldAZDZD`
+		)
+			.then((res) => res.json())
+			.then((resp) => {
+				setFeedsData(resp.data);
+			})
+			.catch((err) => {
+				console.log(err.message);
+			});
 	}, [props.limit]);
-
-	console.log(feeds, "feeds");
 
 	return (
 		<section className="p-5  md:p-20 lg:p-32 grid justify-items-center grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
