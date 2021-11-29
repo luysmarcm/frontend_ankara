99e029bfee59049fe7519e41855240877a31fbb1
luysmarcm <luysmar27@gmail.com>
2021-11-29 17:41:30 -0400

    Modificaciones del SEO

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
             <SeoComponent
                 title='Ankara'
                 description='Desde hace 10 años despertamos tus sentidos con productos y accesorios de belleza para hombres y mujeres. Con presencia en más de 20 estados venezolanos nos hemos consolidado en el mercado nacional para brindarle bienestar y calidad a clientes mayoristas y al detal'
-                image='/imagen/logo.png'
+                image='/imagen/anka.png'
             />
             <main>
                 <Banner />
